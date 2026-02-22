8394a4597a8ccf766e9922d4e698f47b5e8d0dab
luanalauschner <vilarinholuana@gmail.com>
2026-02-22 17:09:44 -0400

    mudança para apenas somente um link

--- app.py
+++ app.py
@@ -969,4 +969,4 @@ def restaurante_cardapio_desativar(item_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
